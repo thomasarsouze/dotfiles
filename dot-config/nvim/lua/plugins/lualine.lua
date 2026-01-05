c9ad8925d423b54cd4dfd23d5f233cc74319313f
thomasarsouze <thomas.arsouze@zoho.com>
2026-01-05 15:20:28 +0100

    Add nvim venv-selector + lualine update

--- dot-config/nvim/lua/plugins/lualine.lua
+++ dot-config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,40 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    options = {
-      sections = {
-        lualine_c = {
-          {
-            "filepath",
-            path = 1,
-            shorting_target = 80, -- adjust as needed
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename", path = 2, shortening_target = 80 },
+          lualine_x = {
+            {
+              function()
+                local venv_path = require("venv-selector").venv()
+                if not venv_path or venv_path == "" then
+                  return ""
+                end
+
+                local venv_name = vim.fn.fnamemodify(venv_path, ":t")
+                if not venv_name or venv_name == "" then
+                  return ""
+                end
+
+                return "🐍 " .. venv_name
+              end,
+              cond = function()
+                return vim.bo.filetype == "python" or vim.bo.filetype == "quarto"
+              end,
+            },
+            "encoding",
+            "filetype",
           },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
-      },
-    },
+      })
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
 }
