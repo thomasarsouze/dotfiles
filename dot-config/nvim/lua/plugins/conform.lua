a72556be3ba08de644149d584a4f18ffa3a6a2e0
thomasarsouze <thomas.arsouze@zoho.com>
2026-01-06 16:40:10 +0100

    LSP config for Python

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,13 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				python = { "ruff_format", "black" }, -- Ruff is faster, Black is more opinionated
-				r = { "styler" }, -- Popular R formatter
-				julia = { "julia_formatter" }, -- Official Julia formatter
-				markdown = { "prettier", "markdownlint" }, -- Prettier for general, markdownlint for linting
-			},
-		},
-	},
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" }, -- "black", Ruff is faster, Black is more opinionated
+        r = { "styler" }, -- Popular R formatter
+        julia = { "julia_formatter" }, -- Official Julia formatter
+        markdown = { "prettier", "markdownlint" }, -- Prettier for general, markdownlint for linting
+      },
+    },
+  },
 }
--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,57 @@
 return {
-  --	{
-  --		"neovim/nvim-lspconfig",
-  --		opts = {
-  --			servers = {
-  --				ruff_lsp = {
-  --					settings = {
-  --						args = {}, -- Optional: customize Ruff
-  --					},
-  --				},
-  --				pyright = {
-  --					settings = {
-  --						python = {
-  --							pythonPath = vim.fn.exepath("python"),
-  --						},
-  --					},
-  --				},
-  --				marksman = {},
-  --				julials = {},
-  --				r_language_server = {
-  --					cmd = { "R", "--slave", "-e", "languageserver::run()" },
-  --				},
-  --			},
-  --		},
-  --	},
+  -- Pyright configuration
+  {
+    "neovim/nvim-lspconfig", -- The plugin you're using for LSPs
+    config = function()
+      -- Pyright setup
+      require("lspconfig").pyright.setup({
+        settings = {
+          pyright = {
+            disableorganizeimports = true,
+          },
+          python = {
+            analysis = {
+              ignore = { "*" }, -- Disable analysis to use ruff for linting
+            },
+          },
+        },
+      })
+
+      -- Ruff LSP setup
+      require("lspconfig").ruff.setup({
+        init_options = {
+          settings = {
+            -- Add ruff-specific settings here if necessary
+          },
+        },
+      })
+    end,
+  },
 }
+
+--return {
+--	{
+--		"neovim/nvim-lspconfig",
+--		opts = {
+--			servers = {
+--				ruff_lsp = {
+--					settings = {
+--						args = {}, -- Optional: customize Ruff
+--					},
+--				},
+--				pyright = {
+--					settings = {
+--						python = {
+--							pythonPath = vim.fn.exepath("python"),
+--						},
+--					},
+--				},
+--				marksman = {},
+--				julials = {},
+--				r_language_server = {
+--					cmd = { "R", "--slave", "-e", "languageserver::run()" },
+--				},
+--			},
+--		},
+--	},
+--}
